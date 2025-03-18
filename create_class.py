0c04369c535e9ddb81cdb0900395660320862cc4
Jan Derick Velez <jderickvelez@gmail.com>
2025-03-18 15:36:08 +0800

    Added class creation and fixed load raw data to DB

---
+++ create_class.py
@@ -0,0 +1,79 @@
+from sqlalchemy import create_engine, text, Column, Integer, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+
+
+DB_URL = "postgresql://postgres:Workeye29@localhost:5432/alljobs"
+engine = create_engine(DB_URL, pool_reset_on_return=None)
+
+Base = declarative_base()
+
+
+class SearchCriteria(Base):
+    __tablename__ = "search_criteria"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    keyword = Column(String, nullable=False)
+    location = Column(String, nullable=True)   
+    
+    rawtable = relationship("RawTable", back_populates="searchcriteria", cascade="all, delete-orphan")
+    # searchresult = relationship("SearchResult", back_populates="searchcriteria    ", cascade="all, delete-orphan")
+class RawTable(Base):
+    __tablename__ = "raw_data_table" 
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    search_criteria_id = Column(Integer, ForeignKey('search_criteria.id'), nullable=False)    
+    raw_data = Column(String, nullable=False)  
+ 
+    searchcriteria = relationship("SearchCriteria", back_populates="rawtable")
+
+
+# class SearchResult(Base):
+#     __tablename__="search_result"
+    
+#     id = Column(Integer, primary_key=True, autoincrement=True )
+#     date_search = Column(DateTime)
+#     date_posted = Column(DateTime)
+#     html_string = Column(String)
+#     is_processed = Column(Boolean)
+#     url_source = Column(String)
+#     search_criteria_id = Column(Integer, ForeignKey('search_criteria.id'))
+    
+#     searchcriteria = relationship("SearchCriteria", back_populates="searchresult")
+#     qualification = relationship("Qualification", back_populates="searchresult", cascade="all, delete-orphan")
+    
+# class Qualification(Base):
+#     __tablename__="qualification"
+    
+#     id = Column(Integer, primary_key=True)
+#     skills_required = Column(String)
+#     years_of_experience = Column(Integer)
+#     company_name = Column(String)
+#     search_result_id = Column(Integer, ForeignKey('search_result.id'))
+    
+#     searchresult = relationship("SearchResult", back_populates="qualification")
+#     benefits = relationship("Benefits", back_populates="qualification", cascade="all, delete-orphan")
+    
+# class Benefits(Base):
+#     __tablename__="benefits"
+    
+#     id = Column(Integer, primary_key=True)
+#     benefits = Column(String)
+#     qualification_id = Column(Integer, ForeignKey('qualification.id'))
+    
+#     qualification = relationship("Qualification", back_populates="benefits")
+#     skillsrequired = relationship("SkillsRequired", back_populates="benefits", cascade="all, delete-orphan")
+    
+# class SkillsRequired(Base):
+#     __tablename__='skills_requred'
+    
+#     id = Column(Integer, primary_key=True)
+#     skill = Column(String)
+#     benefits_id = Column(Integer, ForeignKey('benefits.id'))
+
+#     benefits =  relationship("Benefits", back_populates="skillsrequired")
+
+SessionLocal = sessionmaker(bind=engine)
+session = SessionLocal()
+
+Base.metadata.create_all(engine)
