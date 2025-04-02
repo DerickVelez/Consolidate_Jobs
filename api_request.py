73c3b24508665c4fe0872fce7299278509b36b27
Jan Derick Velez <jderickvelez@gmail.com>
2025-04-02 11:33:40 +0800

    Added working api request

--- api_request.py
+++ api_request.py
@@ -1,14 +1,54 @@
 import requests
+from urllib3.util import Retry
+from requests.adapters import HTTPAdapter
 
-url = ""
+url = "https://job-api.alvinalmodal.dev/job-api-requests"
 
-payload = {""}
+retries = Retry(
+    total= 5,
+    backoff_factor=2,
+    status_forcelist=[
+        429,
+        500,
+        502,
+        503,
+        504
+    ]
+)
+
+adapter = HTTPAdapter(max_retries=retries)
+
+session = requests.Session()
+session.mount("http://", adapter)
+session.mount("https://", adapter)
+
+
+# payload = {
+#   "userId": 1,
+#   "id": 1,
+#   "title": "Mama mo",
+#   "completed": False
+# }
 
 headers = {
     "Content-Type": "application/json"
 }
 
-response = requests.post(url, json=payload, headers=headers)
 
-print(response.status_code)  
-print(response.json())        
+
+
+response = session.get(url,  headers=headers)
+# response = requests.post(url, json=payload, headers=headers)
+
+print(response.status_code)
+print(response.json())
+
+
+
+# response = requests.get(url)
+
+# if response.status_code == 200:  # Check if request was successful
+#     data = response.json()  # Convert response to JSON
+#     print(data)
+# else:
+#     print(f"Error: {response.status_code}")
